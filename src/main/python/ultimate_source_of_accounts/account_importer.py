8774960293df4e72667aa521a7629329265a74b5
thomas-lehmann <thomas.lehmann@immobilienscout24.de>
2015-10-26 17:04:25 +0100

    PIO-118 added tests and additional validation for account_id

--- src/main/python/ultimate_source_of_accounts/account_importer.py
+++ src/main/python/ultimate_source_of_accounts/account_importer.py
@@ -21,10 +21,15 @@ def _check_account_data(accounts):
     if not accounts:
         raise Exception("Account data is empty.")
 
+    all_account_ids = set()
     for account_name, account_data in accounts.items():
+        account_id = account_data.get("id")
+        if account_id in all_account_ids:
+            raise Exception("duplicated id {0} found".format(account_id))
+        all_account_ids.add(account_id)
         if not account_data.get("email"):
             raise Exception("Account data {0} has no email.".format(account_name))
         if "@" not in account_data.get("email"):
             raise Exception("Account data {0} without @ in email.".format(account_name))
-        if not account_data.get("id"):
+        if not account_id:
             raise Exception("Account data {0} has no account id.".format(account_name))
--- src/unittest/python/account_importer_tests.py
+++ src/unittest/python/account_importer_tests.py
@@ -12,28 +12,27 @@ import ultimate_source_of_accounts.account_importer as ai
 
 
 class AccountImportTest(TestCase):
-
     def test_raise_exception_when_account_data_empty(self):
         account_data = {}
         self.assertRaises(Exception, ai._check_account_data, account_data)
 
     def test_raise_exception_when_account_data_without_email(self):
-        account_data = { "account_name": {"id": 42, "email": ""}}
+        account_data = {"account_name": {"id": 42, "email": ""}}
         self.assertRaises(Exception, ai._check_account_data, account_data)
 
-        account_data = { "account_name": {"id": 42}}
+        account_data = {"account_name": {"id": 42}}
         self.assertRaises(Exception, ai._check_account_data, account_data)
 
     def test_raise_exception_when_account_data_with_invalid_email(self):
-        account_data = { "account_name": {"id": 42, "email": "test.testqtest.test"}}
+        account_data = {"account_name": {"id": 42, "email": "test.testqtest.test"}}
         self.assertRaises(Exception, ai._check_account_data, account_data)
 
     def test_raise_exception_when_account_data_without_account_id(self):
-        account_data = { "account_name": {"email": "test.test@test.test"}}
+        account_data = {"account_name": {"email": "test.test@test.test"}}
         self.assertRaises(Exception, ai._check_account_data, account_data)
 
     def test_raise_exception_when_account_data_without_account_id_and_email(self):
-        account_data = { "account_name": {}}
+        account_data = {"account_name": {}}
         self.assertRaises(Exception, ai._check_account_data, account_data)
 
     def test_accept_valid_data(self):
@@ -70,3 +69,23 @@ class AccountImportTest(TestCase):
             self.assertEqual(content, result)
         finally:
             shutil.rmtree(directory)
+
+    def test_raise_exception_when_account_has_the_same_account_id(self):
+        account_data = {"account_name1": {"id": 42, "email": "test.test@test.test"},
+                        "account_name2": {"id": 42, "email": "test2.test2@test.test"}}
+
+        self.assertRaises(Exception, ai._check_account_data, account_data)
+
+    def test_raise_exception_when_two_accounts_have_same_account_id(self):
+        account_data = {"account_name1": {"id": 42, "email": "test.test@test.test"},
+                        "account_name2": {"id": 43, "email": "test2.test2@test.test"},
+                        "account_name3": {"id": 42, "email": "test3.test2@test.test"},
+                        "account_name4": {"id": 43, "email": "test2.test2@test.test"}}
+
+        self.assertRaises(Exception, ai._check_account_data, account_data)
+
+    def test_accounts_with_different_ids(self):
+        account_data = {"account_name1": {"id": 42, "email": "test.test@test.test"},
+                        "account_name2": {"id": 43, "email": "test2.test2@test.test"}}
+        ai._check_account_data(account_data)
+
