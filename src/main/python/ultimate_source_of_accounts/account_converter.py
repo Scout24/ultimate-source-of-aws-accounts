c93f67722baa6b9dc135b51175272eafbd5742ab
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-12-21 15:23:24 +0100

    CLOUD-311 Make pylint happy

--- src/main/python/ultimate_source_of_accounts/account_converter.py
+++ src/main/python/ultimate_source_of_accounts/account_converter.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
+"""Convert AWS account information from internal to external format"""
 
 from __future__ import print_function, absolute_import, division
 import yaml
 import json
 import logging
 
-""" This module exports AWS account information (account id, account name, account email). """
 
 FILENAME = "accounts"
 
 
 def get_converted_aws_accounts(accounts):
-    """ Return converted AWS account data """
+    """Return converted AWS account data.
+
+    Produces a dictionary of "file name" -> "file content" pairs. When
+    uploading to S3, these become "S3 key" -> "S3 value" pairs.
+    """
 
     try:
-        result = {"{filename}.yaml".format(filename=FILENAME): yaml.dump(accounts),
-                  "{filename}.json".format(filename=FILENAME): json.dumps(accounts)}
-    except Exception as e:
-        raise Exception("Failed to convert to yaml and json: {0}".format(e))
+        result = {
+            "{filename}.yaml".format(filename=FILENAME): yaml.dump(accounts),
+            "{filename}.json".format(filename=FILENAME): json.dumps(accounts)}
+    except Exception as exc:
+        raise Exception("Failed to convert to yaml and json: {0}".format(exc))
 
     logging.debug("Data successfully converted to yaml and json")
     return result
