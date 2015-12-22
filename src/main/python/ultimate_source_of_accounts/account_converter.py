cb7a8cf47506936400555086b3bfb5de5b3545f5
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-12-22 13:58:51 +0100

    CLOUD-311 make yaml and json more readable for humans

--- src/main/python/ultimate_source_of_accounts/account_converter.py
+++ src/main/python/ultimate_source_of_accounts/account_converter.py
@@ -16,13 +16,12 @@ def get_converted_aws_accounts(accounts):
     Produces a dictionary of "file name" -> "file content" pairs. When
     uploading to S3, these become "S3 key" -> "S3 value" pairs.
     """
-
     try:
-        result = {
-            "{filename}.yaml".format(filename=FILENAME): yaml.dump(accounts),
-            "{filename}.json".format(filename=FILENAME): json.dumps(accounts)}
+        yaml_data = yaml.dump(accounts, indent=2, default_flow_style=False)
+        json_data = json.dumps(accounts, sort_keys=True, indent=2)
     except Exception as exc:
         raise Exception("Failed to convert to yaml and json: {0}".format(exc))
-
     logging.debug("Data successfully converted to yaml and json")
-    return result
+
+    return {FILENAME + ".yaml": yaml_data,
+            FILENAME + ".json": json_data}
